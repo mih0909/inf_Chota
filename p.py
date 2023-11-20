480076ff23e57b3bfba245ee5a8a27c96522c17f
mih0909@ <mih0909@inbox.ru>
2023-11-20 17:08:15 +0700

    etokomu

--- p.py
+++ p.py
@@ -1,4 +1,5 @@
 def to(chislo, sys):
+	chislo = abs(chislo)
 	A = '0123456789ABCDEF'
 	s = ''
 	while chislo != 0:
@@ -9,17 +10,21 @@ def to(chislo, sys):
 
 def frm(chislo, sys):
 	D = {'0':0, '1':1, '2':2, '3':3,
-	     '4':4, '5':5, '6':6, '7':7,
-	     '8':8, '9':9, 'A': 10, 'B':11,
-	     'C':12, 'D':13, 'E':14, 'F':15}
+	    '4':4, '5':5, '6':6, '7':7,
+	    '8':8, '9':9, 'A': 10, 'B':11,
+	    'C':12, 'D':13, 'E':14, 'F':15}
 	l = len(chislo)
 	n = 0
 	p = 1
 	while l != 0:
 		digit = chislo[l-1]
-		n += D.get(digit) * p	
+		if D.get(digit) > sys:
+			return -1234567890987654321
+		n += D.get(digit) * p
 		l-= 1
 		p*= sys
 	return n
-print(to(10, 2))
-print(frm('13',4))
+a,b = map(int, input('to \n').split())
+d,c = input('frm \n').split()
+print(to(a,b))
+print(frm(d,int(c)))
